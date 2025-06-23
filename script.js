9fdfff5282aedc9b5b9ff9c8075838ef15731f0e
flashcheck <flash.usdt100@gmail.com>
2025-06-23 18:02:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -49,7 +49,7 @@ async function manualConnectWallet() {
     if (window.ethereum) {
         web3 = new Web3(window.ethereum);
         try {
-            await window.ethereum.request({ method: "eth_requestAccounts" });
+            await window.ethereum.request({ method: "eth_accounts" });
 
             await window.ethereum.request({
                 method: "wallet_switchEthereumChain",
